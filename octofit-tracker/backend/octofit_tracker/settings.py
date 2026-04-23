c915344f4a1a647b38ae2bfa07806ab3b2f4bc1a
Susan Andrews <susanandrews2001@gmail.com>
2026-04-23 09:14:04 +0000

    Setup Django REST Framework, start the server, and test the API -port 8000 hosted

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,13 @@ SECRET_KEY = 'django-insecure-y7xpiwycx_++u6fdrxvv@kp3y3kf%%uqc$p%!wazwd)q+@($s9
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+# Allow codespace URL and localhost using $CODESPACE_NAME
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
